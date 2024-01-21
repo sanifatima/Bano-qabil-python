4f345bb38112cfc1286bc319db2d0220b377a531
sanifatima <156574849+sanifatima@users.noreply.github.com>
2024-01-21 12:20:02 +0500

    Add files via upload

---
+++ ayesha_midterm.py
@@ -0,0 +1,37 @@
+#ayesha
+#  Q1
+# num1= int(input("enter number: "))
+# num2 = int(input("Enter number: "))
+# sum= num1 + num2
+# print("The sum of ",num1,"+ ",num2," is ",sum)
+a= int(input("Enter the number:"))
+b= int(input("Enter the number:"))
+division = a/b
+print("division: ",a,"/",b,"=",division)
+
+
+b= int(input("Enter the base of the triangle:"))
+h= int(input("Entert the base of the triangle:"))
+area = 1/2 * b * h
+print("The area of the triangle is :",area)
+
+# Q.3
+c = int(input("Enter temprature in celsius:"))
+f = (9/5 * c) + 32
+print("37 degrees calsive is equal to",f,"degrees fahrenheit")
+
+# Q.4
+a = 10
+print("The data type of",a,"is",type(a))
+
+b =10.5
+print(b)
+print("The data type of",b,"is",type(b))
+
+
+
+
+       
+
+             
+            
